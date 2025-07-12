3774624a530aa8f183d037e62d97e195727e0b67
HoggGogg <godandmorgan@gmail.com>
2025-07-12 16:48:34 +0300

    commit hw1-1

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
-import './App.css'
+import './App.css';
+const coursesTitleArray:  string[] = [
+    'JavaScript Complex',
+    'Java Complex',
+    'Python Complex',
+    'QA Complex',
+    'Fullstack',
+    'Frontend'
+];
 
-function App() {
-
+const App = () => {
 
     return (
-        <></>
+        <>
+        <ul>
+            {
+                coursesTitleArray.map((value, index) => <li key={index}>{value}</li>)
+            }
+        </ul>
+        </>
     );
 }
 
 export default App
+
