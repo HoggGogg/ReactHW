ce8f1c82c47a55275b67b9db244379032c84b628
HoggGogg <godandmorgan@gmail.com>
2025-07-22 18:02:04 +0300

    done hw 3-1

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import './App.css'
+import {Outlet} from "react-router-dom";
+import {Menu} from "./components/menu/Menu.tsx";
 
 function App() {
 
 
     return (
-        <></>
+        <>
+        This is App component
+
+            <Menu/>
+
+            <Outlet/>
+        </>
     );
 }
 
---
+++ src/router/routes.tsx
@@ -0,0 +1,16 @@
+import {createBrowserRouter} from "react-router-dom";
+import App from "../App.tsx";
+import {UsersComponent} from "../components/UsersComponent.tsx";
+import {PostsComponent} from "../components/PostsComponent.tsx";
+import {CommentsComponent} from "../components/CommentsComponent.tsx";
+
+export const routes = createBrowserRouter([
+    {
+        path: "/", element: <App/>, children: [
+            {path: 'users', element: <UsersComponent/>},
+            {path: 'posts', element: <PostsComponent/>},
+            {path: 'comments', element: <CommentsComponent/>},
+            {path: 'products', element: <PostsComponent/>}
+        ]
+    }
+]);
