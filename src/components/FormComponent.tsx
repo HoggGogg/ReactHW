f9d6ed6aa0ecb3f72c13238c1200a9c7a83133a7
HoggGogg <godandmorgan@gmail.com>
2025-07-28 20:21:21 +0300

    lesson  5-2 useForm hook

--- src/components/FormComponent.tsx
+++ src/components/FormComponent.tsx
@@ -1,48 +1,25 @@
-import {type FormEvent, useState} from "react";
+import React from "react";
+import {useForm} from "react-hook-form";
 
 interface IFormProps {
     username: string,
-    password: string
+    password: string,
+    age: number
 }
 
 const FormComponent = () => {
 
-    const [formState, setFormState] = useState<IFormProps>({
-        username: 'foobar',
-        password: '1111'
-    })
+    const {handleSubmit, register} = useForm<IFormProps>();
 
-    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        const user = {
-            username: formState.username,
-            password: formState.password
-        }
-        console.log(user);
+    const costomHandler = (formDataProps: IFormProps) => {
+        console.log(formDataProps);
     };
-
-    // const handleUserNameChange = (e: FormEvent<HTMLInputElement>) => {
-    //     const inputUser = e.target as HTMLInputElement;
-    //     console.log(inputUser.value);
-    //     setFormState({...formState, username: inputUser.username})
-    // };
-    // const handlePasswordChange = (e: FormEvent<HTMLInputElement>) => {
-    //     const inputPass = e.target as HTMLInputElement;
-    //     console.log(inputPass.value);
-    //     setFormState({...formState, password: inputPass.password})
-    // };
-
-    const handleInputChange = (e: FormEvent<HTMLInputElement>) => {
-        const input = e.target as HTMLInputElement;
-        console.log(input.name);
-        setFormState({...formState, [input.name]: input.value})
-    };
-
     return (
         <div>
-            <form onSubmit={handleSubmit}>
-                <input type="text" name={'username'} value={formState.username} onChange={handleInputChange}/>
-                <input type="text" name={'password'} value={formState.password} onChange={handleInputChange}/>
+            <form onSubmit={handleSubmit(costomHandler)}>
+                <input type="text" {...register("username")}/>
+                <input type="text" {...register("password")}/>
+                <input type="number" {...register("age")}/>
                 <button>send</button>
             </form>
         </div>
