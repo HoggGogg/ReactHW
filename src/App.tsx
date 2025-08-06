b7bee103d78d06fb171829ad59d356fbc636378e
HoggGogg <godandmorgan@gmail.com>
2025-08-06 17:28:46 +0300

    done hw 7-1

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import './App.css'
+import {useFetch} from "./hooks/useFetch.tsx";
 
 function App() {
 
+    const users = useFetch<{ id: number, name: string }[]>('https://jsonplaceholder.typicode.com/users', []);
 
     return (
-        <></>
+        <>
+            {
+                users.map((user) => (
+                    <div key={user.id}>
+                        {user.name} : {user.id}
+                    </div>
+                ))
+            }
+        </>
     );
 }
 
